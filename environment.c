a73284e4001f948baaf8ada9b46b2161aba59da9
HamO001 <ndarugastiv9@gmail.com>
2023-07-24 01:22:58 -0700

    retrying

---
+++ environment.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * _myEnviron - prints current environment
+ *
+ * @envp: pointer to environment variable array
+ */
+void _myEnviron(char **envp)
---
+++ execute.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * executeCommand - executes a command passed to the function using /bin/sh
+ * @command: command passes to this function
+ */
+void executeCommand(char *command)
+	{
+		pid_t pid;
+		int status;
+		char *envp[] = {NULL};
+		char *args[4];
+
+		pid = fork();
+
+		if (pid == -1)
+		{
+			perror("fork");
+			return;
+		}
+		else if (pid == 0)
+		{
+			args[0] = "/bin/sh";
+			args[1] = "-c";
+			args[2] = command;
+			args[3] = NULL;
+
+			execve(args[0], args, envp);
+			perror("execve");
+			exit(1);
+		}
+		else
+			waitpid(pid, &status, 0);
+	}
